b3087857fb6e1887ce133d9d2484d8608f13651a
Patryk Tajs <minosyx@gmail.com>
2025-03-29 02:36:42 +0100

    Fix default highlighting

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,15 +7,13 @@ return {
     "tiagovla/tokyodark.nvim",
     {
         "olimorris/onedarkpro.nvim",
-        config = function(_, opts)
-            require("onedarkpro").setup(vim.tbl_deep_extend("force", opts, {
-                highlights = {
-                    ["@variable"] = { fg = "#E18254" },
-                    ["@parameter"] = { fg = "#E18254" },
+        opts = function(_, opts)
+            opts.highlights = {
+                ["@variable"] = { fg = "#E18254" },
+                ["@parameter"] = { fg = "#E18254" },
 
-                    Visual = { bg = "#404754" },
-                },
-            }))
+                Visual = { bg = "#404754" },
+            }
         end,
     },
     {
