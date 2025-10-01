22bfd7016aa8100b665c8c9af4415c3f31c30384
Patryk Tajs <minosyx@gmail.com>
2025-10-01 17:17:43 +0200

    Removed copilot and added Copilot LSP

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,38 +1,39 @@
-return {
-    "zbirenbaum/copilot.lua",
-    optional = true,
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = function(opts)
-        require("copilot.api").status = require("copilot.status")
-        return {
-            suggestion = { enabled = false },
-            panel = { enabled = false },
-            filetypes = {
-                markdown = true,
-                help = true,
-            },
-        }
-    end,
-    config = function()
-        Snacks.toggle({
-            name = "Toggle Copilot",
-            get = function()
-                if not vim.g.copilot_enabled then
-                    return false
-                end
-                return not require("copilot.client").is_disabled()
-            end,
-            set = function(state)
-                if state then
-                    require("copilot").setup()
-                    require("copilot.command").enable()
-                    vim.g.copilot_enabled = true
-                else
-                    require("copilot.command").disable()
-                    vim.g.copilot_enabled = false
-                end
-            end,
-        }):map("<leader>ae")
-    end,
-}
+return {}
+-- return {
+--     "zbirenbaum/copilot.lua",
+--     optional = true,
+--     cmd = "Copilot",
+--     event = "InsertEnter",
+--     opts = function(opts)
+--         require("copilot.api").status = require("copilot.status")
+--         return {
+--             suggestion = { enabled = false },
+--             panel = { enabled = false },
+--             filetypes = {
+--                 markdown = true,
+--                 help = true,
+--             },
+--         }
+--     end,
+--     config = function()
+--         Snacks.toggle({
+--             name = "Toggle Copilot",
+--             get = function()
+--                 if not vim.g.copilot_enabled then
+--                     return false
+--                 end
+--                 return not require("copilot.client").is_disabled()
+--             end,
+--             set = function(state)
+--                 if state then
+--                     require("copilot").setup()
+--                     require("copilot.command").enable()
+--                     vim.g.copilot_enabled = true
+--                 else
+--                     require("copilot.command").disable()
+--                     vim.g.copilot_enabled = false
+--                 end
+--             end,
+--         }):map("<leader>ae")
+--     end,
+-- }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,10 +1,32 @@
 return {
     "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    ---@type PluginLspOpts
     opts = {
         diagnostics = {
             float = {
                 border = "rounded",
             },
         },
+        servers = {
+            copilot = {
+                enabled = false,
+            },
+        },
     },
+
+    config = function(_, opts)
+        local copilot_enabled = vim.lsp.is_enabled("copilot")
+
+        vim.keymap.set("n", "<leader>ae", function()
+            copilot_enabled = not copilot_enabled
+            if copilot_enabled then
+                vim.cmd("LspStart copilot")
+                vim.notify("Copilot LSP enabled")
+            else
+                vim.cmd("LspStop copilot")
+                vim.notify("Copilot LSP disabled")
+            end
+        end, { desc = "Toggle Copilot" })
+    end,
 }
