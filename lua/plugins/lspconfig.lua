3b6202b4df5bbc87e13d6d9d3286cdb5e2c96d05
Patryk Tajs <minosyx@gmail.com>
2026-01-17 04:26:23 +0100

    Create branch for neovim stable dots

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -9,24 +9,24 @@ return {
             },
         },
         servers = {
-            copilot = {
-                enabled = false,
-            },
+            -- copilot = {
+            --     enabled = false,
+            -- },
         },
     },
 
-    init = function()
-        local copilot_enabled = vim.lsp.is_enabled("copilot")
-
-        vim.keymap.set("n", "<leader>ae", function()
-            copilot_enabled = not copilot_enabled
-            if copilot_enabled then
-                vim.cmd("lsp enable copilot")
-                vim.notify("Copilot LSP enabled")
-            else
-                vim.cmd("lsp disable copilot")
-                vim.notify("Copilot LSP disabled")
-            end
-        end, { desc = "Toggle Copilot" })
-    end,
+    -- init = function()
+    --     local copilot_enabled = vim.lsp.is_enabled("copilot")
+    --
+    --     vim.keymap.set("n", "<leader>ae", function()
+    --         copilot_enabled = not copilot_enabled
+    --         if copilot_enabled then
+    --             vim.cmd("lsp enable copilot")
+    --             vim.notify("Copilot LSP enabled")
+    --         else
+    --             vim.cmd("lsp disable copilot")
+    --             vim.notify("Copilot LSP disabled")
+    --         end
+    --     end, { desc = "Toggle Copilot" })
+    -- end,
 }
