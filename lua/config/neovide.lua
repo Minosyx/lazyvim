131858d6a683b2921252ce46d8c636b05a99245f
Patryk Tajs <minosyx@gmail.com>
2026-04-22 10:06:33 +0200

    feat: add new neovide options

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,3 +1,8 @@
 vim.g.neovide_fullscreen = true
 vim.g.neovide_cursor_vfx_mode = { "sonicboom", "pixiedust" }
 vim.g.neovide_underline_stroke_scale = 2
+
+vim.g.neovide_progress_bar_enabled = true
+vim.g.neovide_progress_bar_height = 5.0
+vim.g.neovide_progress_bar_animation_speed = 200.0
+vim.g.neovide_progress_bar_hide_delay = 0.2
