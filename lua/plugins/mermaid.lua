96691d1dae01ae970f85b5efe7e5480392da2b75
Patryk Tajs <minosyx@gmail.com>
2026-05-31 06:06:00 +0200

    feat: add mermaid support

---
+++ lua/plugins/mermaid.lua
@@ -0,0 +1,12 @@
+return {
+    "kevalin/mermaid.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+        require("mermaid").setup({
+            preview = {
+                renderer = "beatiful-mermaid",
+                theme = "dracula",
+            },
+        })
+    end,
+}
