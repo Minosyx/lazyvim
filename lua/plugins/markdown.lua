9e578de2a287fa0c360c72b237197144147ad621
Patryk Tajs <minosyx@gmail.com>
2026-05-31 06:06:00 +0200

    feat: change markdown extensions

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        enabled = false,
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        enabled = true,
+    },
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false,
+        dependencies = { "saghen/blink.cmp" },
+    },
+}
